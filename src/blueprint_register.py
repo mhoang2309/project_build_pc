213da7112fb6d219baf381b3f0e9075f0bb93f15
mhoang2309 <mhoang1998@gmail.com>
2021-10-21 13:36:00 +0700

    Project structure

--- src/blueprint_register.py
+++ src/blueprint_register.py
@@ -1,4 +1,7 @@
 from src.controllers.health_check import health_check_bp
+from src.controllers.user import login_blueprint, logout_bp
 
 def register_blueprint(app):
     app.register_blueprint(health_check_bp, url_prefix="/health_check")
+    app.register_blueprint(login_blueprint, url_prefix="/login")
+    app.register_blueprint(logout_bp, url_prefix="/logout")
--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,11 +1,57 @@
+from flask import Blueprint, make_response, request, jsonify
+from flask.views import MethodView
+
+import jwt
+import datetime
+from functools import wraps
+
+login_blueprint = Blueprint('uesr_blueprint',__name__)
+logout_bp = Blueprint('logout',__name__)
+SECRET_KEY = 'abc'
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('token')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 403
+        try:
+            jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 403
+        return f(*args, **kwargs)
+    return decorated
 
 
-def login(username, password):
-    print(username+'  '+password)
-    return
+class Login(MethodView):
+    def post(self):
+        username = request.form['user']
+        password = request.form['pass']
+        print(username)
+        print(password)
+        if username == 'hoang' and password == '123':
+            token = jwt.encode({'user': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=1000)},
+                            SECRET_KEY)
+            return make_response('OK', 200, {'Token': token})
 
-def logout():
-    return 'logout'
+        return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    
+class Logout(MethodView):
+    @token_required    
+    def get(self):
+        return make_response('OK Logout', 200, {'message':'Logout'})
 
-def register():
-    return 'register'
+
+class Register(MethodView):
+    def put(self):
+        return make_response('200', 200, {'user':'OK'})
+
+
+
+
+login_view = Login.as_view("login_api")
+login_blueprint.add_url_rule("",view_func=login_view, methods=["POST"])
+
+logout_view = Logout.as_view("logout_api")
+logout_bp.add_url_rule("", view_func=logout_view, methods=["GET"])
+
